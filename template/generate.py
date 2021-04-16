d0f987044a2872148761bea08cf300002500c70b
MortalHappiness <b07901069@ntu.edu.tw>
2021-04-16 18:28:24 +0800

    update template

--- template/generate.py
+++ template/generate.py
@@ -19,8 +19,16 @@ if __name__ == "__main__":
         sys.exit(1)
 
     name = sys.argv[1]
-    input_path = os.path.join("input", f"{name}.txt")
-    output_path = os.path.join("output", f"{name}.txt")
+
+    input_dir = "input"
+    output_dir = "output"
+    if not os.path.exists(input_dir):
+        os.mkdir(input_dir)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    input_path = os.path.join(input_dir, f"{name}.txt")
+    output_path = os.path.join(output_dir, f"{name}.txt")
 
     if os.path.exists(input_path):
         print(f"The file {input_path} already exists!")
--- template/test.py
+++ template/test.py
@@ -1,23 +1,14 @@
-#
-# Author: Chi-Sheng Liu
-#
-# ========================================
-
 import unittest
 import subprocess
 import os
 import sys
 
-# ========================================
 
-# ========================================
-
-
-class TestP6(unittest.TestCase):
+class Test(unittest.TestCase):
     def setUp(self):
         self.timeout = 1
         self.maxDiff = None
-        self.script_name = "./bin/" + os.listdir("./bin")[0]
+        self.script_name = os.path.join(".", "bin", os.listdir("./bin")[0])
 
     def run_testcase(self, name):
         with open(os.path.join("input", f"{name}.txt")) as fin:
