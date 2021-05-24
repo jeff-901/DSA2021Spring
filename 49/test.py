5856264df5f5ac1a3c8af402e26d3214c0db1471
jeff-901 <b07901052@ntu.edu.com>
2021-05-24 14:56:01 +0800

    49

---
+++ 49/test.py
@@ -0,0 +1,32 @@
+import unittest
+import subprocess
+import os
+import sys
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.timeout = 1
+        self.maxDiff = None
+        self.script_name = os.path.join(".", "bin", os.listdir("./bin")[0])
+
+    def run_testcase(self, name):
+        with open(os.path.join("input", f"{name}.txt")) as fin:
+            input_data = fin.read()
+        with open(os.path.join("output", f"{name}.txt")) as fin:
+            ans = fin.read()
+        output = subprocess.check_output([self.script_name], text=True,
+                                         timeout=self.timeout, input=input_data)
+        self.assertEqual(output, ans)
+
+    def test_01_sample(self):
+        self.run_testcase("1")
+    
+    def test_02_sample(self):
+        self.run_testcase("2")
+    
+    def test_03_sample(self):
+        self.run_testcase("3")
+
+if __name__ == "__main__":
+    unittest.main()
